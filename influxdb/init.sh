618b4efcb32d6e4bfcd8e9ff931545db35ded53f
David Lennick <david.lennick@gmail.com>
2019-10-22 22:18:20 -0400

    last commit before merge lol

--- influxdb/init.sh
+++ influxdb/init.sh
@@ -2,10 +2,15 @@
 
 sudo -S apt install -y influxdb-client
 
-helm install \
-  --name influxdb-deploy \
-  -f values.yaml \
-  stable/influxdb
+# helm install \
+#   --name influxdb-deploy \
+#   -f values.yaml \
+#   stable/influxdb
+# there was an issue with the helm chart, the config file wasn't valid
+
+
+kubectl apply -f influxdb.deploy.yaml
+
 
 sleep 5
 
